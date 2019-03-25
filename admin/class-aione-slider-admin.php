60ca2727f3eaff0c642a4e53e4c1ed4c9d9236ae
SGS Sandhu <sgs.sandhu@gmail.com>
2019-03-25 20:37:04 +0530

    Update

--- admin/class-aione-slider-admin.php
+++ admin/class-aione-slider-admin.php
@@ -83,7 +83,7 @@ class Aione_Slider_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/aione-slider-admin.css', array(), $this->version, 'all' );
+		// wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/aione-slider-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -106,7 +106,7 @@ class Aione_Slider_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/aione-slider-admin.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/aione-slider-admin.js', array( 'jquery' ), $this->version, false );
 
 	}
 
